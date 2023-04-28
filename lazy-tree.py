8efa4666c8725b930c06bdf8ad4e64aa38025149
thelazyone <giacomo.pantalone@gmail.com>
2023-04-28 11:06:33 +0300

    Implemented real time update for the tree

--- lazy-tree.py
+++ lazy-tree.py
@@ -19,6 +19,7 @@ import random
 import math
 from mathutils import Vector, Quaternion
 
+
 class Section:
     def __init__(self, points, depth, weight, open_end=True):
         self.points = points
@@ -26,17 +27,24 @@ class Section:
         self.depth = depth
         self.weight = weight
         
+            
+
+def update_tree():
+    bpy.ops.growtree.create_tree()
+
 # Blender classes:        
 class GROWTREE_PG_tree_parameters(bpy.types.PropertyGroup):
-    seed: bpy.props.IntProperty(name="Seed", default=0)
-    iterations: bpy.props.IntProperty(name="Iterations", default=20, min=0, max=256)
-    split_chance: bpy.props.FloatProperty(name="Split Chance", default=0.05, min=0, max=1)
-    split_angle: bpy.props.FloatProperty(name="Split Angle", default=45, min=0, max=90)
-    light_source: bpy.props.FloatVectorProperty(name="Light Source", default=(0, 0, 1000))
-    light_searching: bpy.props.FloatProperty(name="Light Searching", default=0.5, min=-1, max=2)
-    trunk_gravity: bpy.props.FloatProperty(name="Trunk Gravity", default=0.5, min=-1, max=2)
-    split_ratio_bottom: bpy.props.FloatProperty(name="Branch Split Ratio Bottom", default=0.4, min=0.01, max=0.5)
-    split_ratio_top: bpy.props.FloatProperty(name="Branch Split Ratio Top", default=0.1, min=0.01, max=0.5)
+    seed: bpy.props.IntProperty(name="Seed", default=0, update=update_tree)
+    iterations: bpy.props.IntProperty(name="Iterations", default=20, min=0, max=256, update=update_tree)
+    split_chance: bpy.props.FloatProperty(name="Split Chance", default=0.05, min=0, max=1, update=update_tree)
+    split_angle: bpy.props.FloatProperty(name="Split Angle", default=45, min=0, max=90, update=update_tree)
+    light_source: bpy.props.FloatVectorProperty(name="Light Source", default=(0, 0, 1000), update=update_tree)
+    light_searching: bpy.props.FloatProperty(name="Light Searching", default=0.5, min=-1, max=2, update=update_tree)
+    ground_avoiding: bpy.props.FloatProperty(name="Ground Avoiding", default=0.5, min=-1, max=2, update=update_tree)
+    trunk_gravity: bpy.props.FloatProperty(name="Trunk Gravity", default=0.5, min=-1, max=2, update=update_tree)
+    split_ratio_bottom: bpy.props.FloatProperty(name="Branch Split Ratio Bottom", default=0.4, min=0.01, max=0.5, update=update_tree)
+    split_ratio_top: bpy.props.FloatProperty(name="Branch Split Ratio Top", default=0.1, min=0.01, max=0.5, update=update_tree)
+
 
 
 class GROWTREE_OT_create_tree(bpy.types.Operator):
@@ -48,6 +56,15 @@ class GROWTREE_OT_create_tree(bpy.types.Operator):
 
     def execute(self, context):
         tree_parameters = context.scene.tree_parameters
+                        
+        # Before calculating a new tree, making sure that the amount of segments isn't 
+        # likely to exceed a certain amount. let's say 10000.
+        max_segments = 10000 
+        est_segments = pow(1 + tree_parameters.split_chance, tree_parameters.iterations)
+        if  est_segments > max_segments:
+            self.report({'WARNING'}, \
+                f"Estimated number of segments ({int(est_segments)}) exceeds the limit ({max_segments}). Aborting.")
+            return {'CANCELLED'}
         
         mesh = self.create_tree_mesh(tree_parameters)
         obj_name = "Created Tree"
@@ -59,9 +76,12 @@ class GROWTREE_OT_create_tree(bpy.types.Operator):
         bpy.context.collection.objects.link(obj)
 
         return {'FINISHED'}
+    
 
     def create_tree_mesh(self, tree_parameters):
-
+        
+        random.seed(tree_parameters.seed)
+    
         def get_random_value(seed):
             random.seed(seed)
             return random.random()
@@ -90,19 +110,7 @@ class GROWTREE_OT_create_tree(bpy.types.Operator):
 
                     # Direction is provided by the last segment in the section. Sections start with
                     # two points so we can assume that there are at least 2 points here.
-                    direction = get_direction(section.points[-2], section.points[-1])
-                    
-                    # The split direction is with a given angle against the product of the 
-                    # original direction with a random one.
-                    split_direction = direction.copy()
-                    random_direction = Vector((random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1))).normalized()
-                    light_direction = Vector((tree_parameters.light_source[0], tree_parameters.light_source[1], tree_parameters.light_source[2])).normalized()
-                    
-                    # Combining the random direction with the light direction.
-                    random_direction = (random_direction * (1 - tree_parameters.light_searching) + light_direction * tree_parameters.light_searching).normalized()
-                    
-                    # Rotating the split branch direction
-                    split_direction.rotate(Quaternion(direction.cross(random_direction).normalized(), math.radians(tree_parameters.split_angle)))
+                    initial_direction = get_direction(section.points[-2], section.points[-1])
 
                     # Calculating the weight of the two branches. The distribution goes from 0 to
                     # 0.5 (equal split). the new branch is always the smaller one.
@@ -123,12 +131,67 @@ class GROWTREE_OT_create_tree(bpy.types.Operator):
                         weight=section.weight * (split_ratio), \
                         open_end=True)
 
-                    new_section1.points.extend([new_section1.points[-1] + direction])
-                    new_section2.points.extend([new_section2.points[-1] + split_direction])
+                    # Section A is the main section and tends to follow the previous branch. 
+                    # We then generate two random directions for the branches. how much that affects the 
+                    # final direction is based on the weight.
+                    direction1 = initial_direction.copy()
+                    direction2 = initial_direction.copy()
+                    random_direction = get_random_direction(tree_parameters)
+                    #direction1 = direction1 * new_section1.weight
+                    #direction1 = (direction1 + random_direction * new_section2.weight).normalized()
+                    angle1 = -tree_parameters.split_angle * split_ratio
+                    angle2 = tree_parameters.split_angle * (1 - split_ratio)
+                    direction1.rotate(Quaternion(direction1.cross(random_direction).normalized(), math.radians(angle1)))
+                    direction2.rotate(Quaternion(direction2.cross(random_direction).normalized(), math.radians(angle2)))
+
+                    new_section1.points.extend([new_section1.points[-1] + direction1])
+                    new_section2.points.extend([new_section2.points[-1] + direction2])
 
                     new_sections.extend([new_section1, new_section2])
 
             return new_sections
+
+        def get_random_direction(tree_parameters):
+            
+            # Random direction is calculated in theta-phi to get a more uniform distribution.
+            random_direction = uniform_random_direction()
+
+            # Combining the random direction with the light direction
+            light_direction = Vector((tree_parameters.light_source[0], tree_parameters.light_source[1], tree_parameters.light_source[2])).normalized()
+            
+            # Adding the various effects and normalizing.
+            random_direction = random_direction + \
+                light_direction * tree_parameters.light_searching
+            random_direction = random_direction.normalized()
+
+            # Avoiding ground means that all negative Z values of direction are mitigated.
+            random_direction.z = avoid_ground(random_direction.z, tree_parameters.ground_avoiding)
+            return random_direction.normalized()
+            
+
+        def avoid_ground(value, parameter):
+
+            # Quadratic logic. If parameter is < 1 the effect is mostly
+            # on the negative elements.
+            a = 1 - parameter
+            b = parameter * parameter
+
+            if value < 0:
+                return a * value + b * value * value
+            else:
+                return value + b * value * value
+
+
+        def uniform_random_direction():
+            theta = random.uniform(0, 2 * math.pi)
+            phi = random.uniform(0, math.pi)
+            
+            x = math.sin(phi) * math.cos(theta)
+            y = math.sin(phi) * math.sin(theta)
+            z = math.cos(phi)
+            
+            return Vector((x, y, z)).normalized()
+
 
         mesh = bpy.data.meshes.new("Tree")
         bm = bmesh.new()
